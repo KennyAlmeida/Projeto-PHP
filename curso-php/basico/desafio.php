ec58da00eec7ba57cf3af253578902fe341eb202
kennyalmeida25 <kenny_almeida_25@hotmail.com>
2020-06-07 03:01:47 -0300

    Modulo 1 - Basico

---
+++ curso-php/basico/desafio.php
@@ -0,0 +1,8 @@
+<div class="titulo">Desafio</div>
+
+
+<ul>
+	<li><?= "1+1"; ?>=2</li>
+	<li>4+4=<?php echo 4+4; ?></li>
+	<li>8+8=<?=8+8;?></li>
+</ul>
---
+++ curso-php/basico/ola.php
@@ -0,0 +1,16 @@
+<div class="titulo">Olá PHP</div>
+
+
+<h2>Exemplo A</h2>
+<?php
+//echo 'Olá <br>';
+echo 'Olá';
+echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+<?= "Outra forma de me 'expressar'!"?>
+
+<h2>Exemplo C</h2>
+<?php
+phpinfo();
